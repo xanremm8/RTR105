3657d78acad73bdd517488700093feaf74ef5764
xanremm8 <43029563+xanremm8@users.noreply.github.com>
2018-10-01 15:37:17 +0300

    Add files via upload

---
+++ python2901218.py
@@ -0,0 +1,33 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> a
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    a
+NameError: name 'a' is not defined
+>>> a = a
+
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    a = a
+NameError: name 'a' is not defined
+>>> a=1
+>>> a
+1
+>>> a = 20
+>>> b =55
+>>> r = c
+
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    r = c
+NameError: name 'c' is not defined
+>>> r=c
+
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    r=c
+NameError: name 'c' is not defined
+>>> 
---
+++ test_1.la.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+
+a = 1
+b = 1.89
+c = 'D'
+# https://www.cplusplus.xom/reference/cstdio/print//
+print("a = %b"%(a))
+print("b = %4f(b))
+print("c = %s"%(c))
+
+
+
+
+
+
